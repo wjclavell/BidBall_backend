b3e824ecb4ae4d65f0f754120c96930a92cc3aa5
William Clavell <wjclavell28@gmail.com>
2020-09-16 15:39:41 -0400

    CORS configuration

--- p4backend/settings.py
+++ p4backend/settings.py
@@ -40,9 +40,11 @@ INSTALLED_APPS = [
     'rest_framework',  # this is for our restAPI
     'apps.authentication',
     'apps.api',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -51,6 +53,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'p4backend.urls'
 
