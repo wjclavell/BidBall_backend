b8267c1c114629a0f775ec451787458a28834ef0
William Clavell <wjclavell28@gmail.com>
2020-09-24 22:34:49 -0400

    increased pagination amount

--- p4backend/settings.py
+++ p4backend/settings.py
@@ -132,7 +132,7 @@ REST_FRAMEWORK = {
     ),
     # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 3
+    'PAGE_SIZE': 7
 }
 
 # Overriding DRF JWT settings
